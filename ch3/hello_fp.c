b3e64a9e8dbd68767e64a69a4af345b39b2f871b
aokblast <i19780219111@kimo.com>
2021-07-25 23:06:27 +0800

    Add hello_fp.c and hello_int.c

---
+++ ch3/hello_fp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    double a = 1.5;
+    double b = 1.6;
+    double c;
+    c = a + b;
+}
---
+++ ch3/hello_int.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+    int64_t a = 1;
+    int64_t b = 2;
+    int64_t c = a + b;
+}
