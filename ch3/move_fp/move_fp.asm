226dd85334692f7290c38d030448bf151e8e1ac7
aokblast <i19780219111@kimo.com>
2021-07-26 23:54:32 +0800

    Change move_fp.asm

--- ch3/move_fp/move_fp.asm
+++ ch3/move_fp/move_fp.asm
@@ -1,18 +1,67 @@
+default rel
+extern printf
 segment .data
-    float1 dq 1.5
+    float1 dd 1.5
+    formatfp db "%lf", 0xA, 0
+    formatin db "%d", 0xA, 0
+    int123 dd 2
 segment .bss
 
 segment .text
     global asm_main
 
 asm_main:
-    mov rsi, float1
-    call print 
+
+    movsd xmm0, [float1] ; call ftod
+    push rbx
+    call ftod
+    pop rbx
+
+    lea rdi, [formatfp] ; call printf
+    mov ax, 1
+    push rbx
+    call printf
+    pop rbx
+
+    mov eax, [int123] ; call itod
+    push rbx
+    call itod
+    pop rbx
+
+    lea rdi, [formatfp] ; call printf
+    mov ax, 1
+    push rbx
+    call printf
+    pop rbx
+
+    movsd xmm0, [float1] ; call d2i
+    push rbx
+    call ftoi
+    pop rbx
+
+    lea rdi, [formatin] ; call printf
+    mov ax, 0
+    push rbx
+    call printf
+    pop rbx
+
+    mov rax, 0 ; ret
     ret
 
-print:
-    mov rax, 0x1
-    mov rdi, 0x1
-    mov rdx, 0x8
-    syscall
+ftod:
+    movss	[rsp-4], xmm0 
+    vpxor xmm0, xmm0
+    cvtss2sd xmm0, [rsp-4]
+    ret
+
+itod:
+    mov	[rsp-4], eax 
+    vpxor xmm0, xmm0
+    cvtsi2sd xmm0, [rsp-4]
+    ret
+
+ftoi:
+    movss	[rsp-4], xmm0 
+    vpxor xmm0, xmm0
+    cvttss2si rsi, [rsp-4]
     ret
