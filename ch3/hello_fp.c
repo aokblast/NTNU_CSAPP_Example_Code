191dd7e47eb9f4850637f73741fd46c319e15393
aokblast <i19780219111@kimo.com>
2021-07-26 22:37:29 +0800

    Remove all redundant file

--- ch3/hello_fp.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    double a = 1.5;
-    double b = 1.6;
-    double c;
-    c = a + b;
-}
--- ch3/hello_int.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-int main(){
-    int64_t a = 1;
-    int64_t b = 2;
-    int64_t c = a + b;
-}
