1e6aa7b18411a16a3aa7b3556cce38c17b6b8737
jw910731 <jw910731@gmail.com>
2021-07-27 01:35:14 +0800

    Modify float cmp to use scanf as compare num input

--- ch3/float_compare/float_cmp.s
+++ ch3/float_compare/float_cmp.s
@@ -1,10 +1,20 @@
     .text
     .global asm_main
 asm_main:
-    # load number
-    movq (a), %xmm0
-    movq (b), %xmm1
-    ucomisd %xmm0, %xmm1 # perform compare and set EFLAG
+    subq $16, %rsp # setup stack
+    # Input number
+    pushq %rax # setup alignment
+    leaq input_prompt, %rdi
+    call printf # call printf to print prompt
+    leaq input_format, %rdi
+    leaq 8(%rsp), %rsi # additional offset to avoid touching alignment filler
+    leaq 16(%rsp), %rdx
+    call scanf # call scanf to get user input
+    popq %rax # remove alignment
+    # load number from mem
+    movq (%rsp), %xmm0
+    movq 8(%rsp), %xmm1
+    ucomisd %xmm1, %xmm0 # perform compare and set EFLAG (S2 ? S1)
     pushq %rax # perform call alignment first
     # lesser equal branch start
     ja great_label
@@ -18,11 +28,14 @@ great_label:
     call puts
 end:
     popq %rax # revoke call alignment
+    add $16, %rsp # restore stack frame
     xor %rax, %rax
     ret
 
     .data
 a: .double 1.3
 b: .double 2.5
-greater: .asciz "Greater\n"
-lesser: .asciz "Lesser\n"
+greater: .asciz ">"
+lesser: .asciz "<"
+input_prompt: .asciz "Input two float number: "
+input_format: .asciz "%lf %lf"
