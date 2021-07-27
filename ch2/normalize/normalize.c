b38656b9b66f224443d3e170787dc8b857dc051f
aokblast <i19780219111@kimo.com>
2021-07-27 23:28:50 +0800

    Change noemalize.c

--- ch2/normalize/normalize.c
+++ ch2/normalize/normalize.c
@@ -5,13 +5,15 @@
 union {
     double dval;
     uint64_t hval;
-} InfVal, NanVal, NormVal;
+} InfVal, NanVal, NormVal, zVal;
 
 int main(){
     InfVal.dval = INFINITY;
     NanVal.dval = NAN;
     NormVal.dval = 3.1415;
+    zVal.dval = 0.0;
     printf("Dec: %lf Hex: %lx\n", InfVal.dval, InfVal.hval);
     printf("Dec: %lf Hex: %lx\n", NanVal.dval, NanVal.hval);
     printf("Dec: %lf Hex: %lx\n", NormVal.dval, NormVal.hval);
+    printf("Dec: %lf Hex: %lx\n", zVal.dval, zVal.hval);
 }
